7ce148b0a55ef6039df26ef009ddff30faebb457
Student Aman4580 <amanverma7124@gmail.com>
2024-08-01 13:46:45 +0530

    Leetcode 1388 Recursion

---
+++ Recursion2/PizzaWith3nSlices.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solve(vector<int>& slices, int s, int e, int n) {
+    if (n == 0 || s > e) {
+        return 0;
+    }
+    int include = slices[s] + solve(slices, s + 2, e, n - 1);
+    int exclude = 0 + solve(slices, s + 1, e, n);
+    return max(include, exclude);
+}
+
+int maxSizeSlices(vector<int>& slices) {
+    int k = slices.size();
+    int opt1 = solve(slices, 0, k - 2, k / 3);
+    int opt2 = solve(slices, 1, k - 1, k / 3);
+    return max(opt1, opt2);
+}
+
+int main() {
+    vector<int> slices = {1, 2, 3, 4, 5, 6}; 
+    int result = maxSizeSlices(slices);
+    return 0;
+}
