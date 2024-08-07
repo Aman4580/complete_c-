676138f1899ba347a23773d08130482caf8defd2
Student Aman4580 <amanverma7124@gmail.com>
2024-08-07 23:46:31 +0530

    commit

--- prefixSum/arraypartation.cpp
+++ prefixSum/arraypartation.cpp
@@ -21,7 +21,7 @@ int main(){
     
     for(int i = 0; i < ans.size(); i++){
         for(int j = 0; j < ans[i].size(); j++){
-            cout << ans[i][j];
+            cout << ans[i][j]<<" ";
         }
         cout << endl;
     }
