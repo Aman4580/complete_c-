44ee682e3dd1435ff85cfe65ff920d143d6bc183
Student Aman4580 <amanverma7124@gmail.com>
2024-08-16 23:55:18 +0530

    commit

---
+++ Trie/tre.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str = "()(()())()()(())";
+    stack<char> st;
+
+    for (int i = 0; i < str.size(); i++) {
+        if (str[i] == '(') {
+            st.push('(');
+        } else if (str[i] == ')') {
+            if (!st.empty() && st.top() == '(') {
+                st.pop();
+            } else {
+                cout << "Not Valid parenthesis" << endl;
+                return 0;
+            }
+        }
+    }
+
+    if (st.empty()) {
+        cout << "Valid parenthesis" << endl;
+    } else {
+        cout << "Not Valid parenthesis" << endl;
+    }
+
+    return 0;
+}
