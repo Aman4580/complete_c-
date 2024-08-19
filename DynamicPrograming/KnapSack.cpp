a87ad751323440def68d71c96a1032d0b52ac17a
Student Aman4580 <amanverma7124@gmail.com>
2024-08-20 03:28:33 +0530

    Dp Knap Sack Problem

--- DynamicPrograming/KnapSack.cpp
+++ DynamicPrograming/KnapSack.cpp
@@ -21,6 +21,22 @@ if(capacity>=weight[index]){
 int exc = 0+solveBottomUp(value,index+1,weight,capacity,dp);
 return dp[index][capacity] = max(inc,exc);
 }
+int solveTopBottom(vector<int>& value, vector<int>& weight, int capacity, vector<vector<int>>& dp) {
+    int n = value.size(); 
+    
+    for (int i = n - 1; i >= 0; i--) {
+        for (int c = 1; c <= capacity; c++) {
+            int inc = 0;
+            if (c >= weight[i]) {
+                inc = value[i] + dp[i + 1][c - weight[i]];
+            }
+            int exc = dp[i + 1][c];
+            dp[i][c] = max(inc, exc);
+        }
+    }
+    return dp[0][capacity];
+}
+
 
 int main (){
     vector<int>val,wt;
@@ -32,10 +48,15 @@ int main (){
     wt.push_back(20);
     wt.push_back(30);
     int capacity = 50;
-    int n =3;
+    int n = 3;
     vector<vector<int>> dp(n+1, vector<int>(capacity + 1, -1));
+    vector<vector<int>> dp1(n+1, vector<int>(capacity + 1, 0));
     
     cout<<"answer is this "<< " "<<solveRecursion(val,0,wt,capacity)<<endl;
     
     cout<<"answer is this "<< " "<<solveBottomUp(val,0,wt,capacity,dp)<<endl;
+
+    cout<<"answer is this "<< " "<<solveTopBottom(val,wt,capacity,dp1)<<endl;
+
+   
 } 
