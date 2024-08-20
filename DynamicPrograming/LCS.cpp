4e9eb7fcdc9c4a26fcb751e90075aca9da77f3e8
Student Aman4580 <amanverma7124@gmail.com>
2024-08-20 06:34:10 +0530

    LCS

--- DynamicPrograming/LCS.cpp
+++ DynamicPrograming/LCS.cpp
@@ -1,22 +1,48 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-// bottom up dp
-vector<vector<int>>dp;
-int lcs(string &s1,string &s2,int i,int j){
-if(i>=s1.size()) return 0;
-if(j>=s2.size()) return 0;
-if(dp[i][j]!=-1) return dp[i][j];
-if(s1[i]==s2[j]){
-    return dp[i][j]= 1+lcs(s1,s2,i+1,j+1);
-}else{
-    return dp[i][j]=max(lcs(s1,s2,i+1,j),lcs(s1,s2,i,j+1));
+
+// Global dp array
+vector<vector<int>> dp;
+
+// Function to compute the length of LCS
+int lcs(string &s1, string &s2, int i, int j) {
+    if (i >= s1.size() || j >= s2.size()) return 0;
+    if (dp[i][j] != -1) return dp[i][j];
+    if (s1[i] == s2[j]) {
+        return dp[i][j] = 1 + lcs(s1, s2, i + 1, j + 1);
+    } else {
+        return dp[i][j] = max(lcs(s1, s2, i + 1, j), lcs(s1, s2, i, j + 1));
+    }
 }
+
+// Function to reconstruct the LCS
+string printLcs(string &s1, string &s2, int i, int j) {
+    if (i >= s1.size() || j >= s2.size()) return "";
+    if (s1[i] == s2[j]) {
+        return s1[i] + printLcs(s1, s2, i + 1, j + 1);
+    } else {
+        if (dp[i + 1][j] > dp[i][j + 1]) {
+            return printLcs(s1, s2, i + 1, j);
+        } else {
+            return printLcs(s1, s2, i, j + 1);
+        }
+    }
 }
-int main(){
+
+int main() {
     dp.clear();
-    dp.resize(1005,vector<int>(1005,-1));
-    string s1,s2;
-    cin>>s1>>s2;
-    cout<< lcs(s1,s2,0,0);
+    dp.resize(1005, vector<int>(1005, -1));
+
+    string s1 = "abcdabc";
+    string s2 = "bcbcda";
+
+    // Compute the length of LCS
+    int lcsLength = lcs(s1, s2, 0, 0);
+    cout << "Length of LCS: " << lcsLength << endl;
+
+    // Print the LCS
+    string lcsString = printLcs(s1, s2, 0, 0);
+    cout << "LCS: " << lcsString << endl;
+
     return 0;
 }
