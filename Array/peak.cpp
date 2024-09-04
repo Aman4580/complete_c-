330121348381619591a739ffec5032cbadf6f75d
Student Aman4580 <amanverma7124@gmail.com>
2024-09-04 23:00:50 +0530

    commit

---
+++ Array/peak.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>v = {1,4,6,1,2,10};
+    int ans = INT_MAX;
+    for(int i=1;i<v.size()-1;i++){
+        if(v[i]>v[i-1] && v[i]<v[i+1]){
+            ans = min(ans,v[i]);
+        }
+    }
+    cout<<" The answer is this"<<ans;
+    return 0;
+}
--- Array/special.cpp
+++ Array/special.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int solve(int n) {
-    if(n == 0) return 0;
+    if(n == 0) return 1;
     if(n == 1) return 1;
-    int a1 = solve(n-1);
-    int a2 = solve(n-2);
-    return a1*a1+a2*a2;
+
+    int ans = ((solve(n-1)*solve(n-1))+(solve(n-2)+solve(n-2)));
+    return ans%47;
 }
 
 int main() {
